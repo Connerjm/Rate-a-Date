8606529f656fde6396043338f93bfba8f4ae87e5
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-18 16:27:06 -0700

    added nav header, working on the components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,18 +1,23 @@
 import React from "react";
+import "./nav.css";
 
 function NavBar() {
     
 
     return (
         <header className="header">
-            <button className="home"></button>
+            {/*<button className="home"></button>*/}
             <div className="row">
-                <div></div>
-                <div className="logo">
+                <div className="col-md-4"></div>
+                <div className="col-md-4">
+                    <img src="./spark.png" alt="logo" />
+                </div>
+                <div className="col-md-4 flex-row" id="pages">
+                    <a href="/SignupSignin" id="login">Login</a>
+                    <a href="/SignupSignin" id="signup">Signup</a>
                 </div>
             </div>
-            <a href="/" id="login">Login</a>
-            <a href="/" id="logout">Signup</a>
+            
              {/* These will be on the other pages just added to see what we need
                 <a href="/" id="profile">Profile</a>
                 <a href="/" id="signout">Sign Out</a>
