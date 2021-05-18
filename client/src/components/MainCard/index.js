f10ca2307cce4cfb103acd9a10a23580f36a8569
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-18 16:26:13 -0700

    added main card component, setting up home page card

---
+++ client/src/components/MainCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../Hero"
+
+const MainCard = () => {
+  return (
+    <div>
+        <section className="card" id="mainCard">
+          <Hero />
+        </section>
+    </div>
+  );
+};
+
+export default MainCard;
