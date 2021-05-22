c58624cb99ce95a71b7efe6743d15580a325c037
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-22 09:58:56 -0700

    added some handlers for setting up click functions

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,35 @@
-import React from "react";
-import MainCard from "../components/MainCard"
-import Footer from "../components/Footer"
+import React, { Component } from "react";
+import MainCard from "../components/MainCard";
+import Footer from "../components/Footer";
+import { Container } from "../components/Grid";
 
-const Home = () => {
-  return (
-    <div>
+
+class Home extends Component {
+    state = {
+      search: "",
+      error: "",
+      message: ""
+    };
+
+  //function to take value of what enter in the search bar
+  handleInputChange = event => {
+    this.setState({ search: event.target.value })
+  };
+  //function to control the submit button of the search form 
+  handleFormSubmit = event => {
+    event.preventDefault();
+  }
+  
+  render() {
+    return (
+      <Container fluid>
         <MainCard />
         <Footer />
-    </div>
-  );
-};
+      </Container>
+    );
+  }
+  
+}
+
 
 export default Home;
