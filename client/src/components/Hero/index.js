2d531d8d6f4bba4fe0ac0c36d2b4d537600f6d3a
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-22 09:52:33 -0700

    updated the hero for search component and deleted files that weren't needed

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import "./hero.css";
+import "./style.css";
 
-function Hero() {
-    
-
+const Hero = props => {
     return (
         <section className="heroImg"> 
             <p className="para">Date ideas to turn the spark into a flame</p>
             <form className="searchForm flex">
-                <input type="search" className="search" placeholder="Search for ideas" />
-                <a href="/" className="go"><i className="fa fa-search" id="sIcon"></i></a>
+                <div className="wrapper"><input className="search"
+                    value={props.search}
+                    placeholder="Search for ideas"
+                    type="text"
+                    name="searchIdeas"
+                    onChange={props.handleInputChange}></input>
+                    <a href="/" className="go"><i className="fa fa-search" id="sIcon"></i></a>
+                    </div>  
             </form>
         </section>
     );
