7ddc6195277fac75be0b5bdb0994282771193cc1
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-05-25 18:49:15 -0700

    added profile, details, signup and started results

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react";
 import Footer from "../components/Footer";
 import { Container} from "../components/Grid";
+import Details from "../components/DetailsCard"
 
 
 class Detail extends Component {
     render() {
         return (
           <Container fluid>
-              <p>In the process</p>
+              <Details />
             <Footer />
           </Container>
         );
