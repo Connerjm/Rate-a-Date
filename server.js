cac37ef79199e5c4e9df9a843314e8e756eba1ff
Rikio Campbell <rikiocamp@gmail.com>
2021-05-25 19:49:25 -0700

    added config folder, user model, validation folder, user seed, updated server.js, added user route

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -23,7 +34,14 @@ mongoose.connect(
         useCreateIndex: true,
         useFindAndModify: false
     }
-);
+).then(() => console.log("MongoDB successfully connected"));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 app.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
